c0122212b4900941669743184509919b2dfcdbc8
Aleksandr Beliaev <trap000d@gmail.com>
2016-10-04 15:49:09 +1300

    Fixed unicode file name encoding issues. Created a stub for upload feature

--- KUAL/seafile/bin/sfcli.py
+++ KUAL/seafile/bin/sfcli.py
@@ -2,6 +2,8 @@
 # -*- coding:utf-8 -*-
 
 import requests
+from requests.packages.urllib3.exceptions import SubjectAltNameWarning
+
 import ConfigParser
 import os
 import shutil
@@ -9,6 +11,16 @@ from subprocess import call
 
 ### Some global definitions
 cfg_dir='/mnt/us/extensions/seafile'
+
+def spinning_cursor():
+    while True:
+        for cursor in '|/-\\':
+            yield cursor
+
+def spinner_up(s):
+    call(['eips', '1', str(max_y-1),' '])
+    call(['eips', '1', str(max_y-1), s ] )
+    return;
 
 def safe_str(obj):
     """ return the byte string representation of obj """
@@ -18,6 +30,11 @@ def safe_str(obj):
         # obj is unicode
         return unicode(obj).encode('unicode_escape')
 
+def safe_unicode(str):
+    try:
+        return str.decode('utf-8')
+    except UnicodeEncodeError:
+        return str
 
 def cprint(s, ypos):
     call(['eips', '3 ', str(ypos+max_y-3), safe_str(s)[:max_x-4] ] )
@@ -73,12 +90,13 @@ def sf_ls_lib(dir_entry='/'):
     r = requests.get( url + '/api2/repos/' + libid + '/dir/?p=' + dir_entry, headers = hdr, verify=ca_verify)
     return r.json();
 
-# Returns 4 lists: 0 - with directories to erase, 1 - with filenames to erase, 2 - for files to download 3 - for hashes to update
+# Returns 5 lists: 0 - with directories to erase, 1 - with filenames to erase, 2 - for files to download 3 - for hashes to update 4 - local files to upload
 def sf_get_modified(dir_entry='/'):
     h_lcl={}
     h_srv={}
     h1=[]
     h2=[]
+    fl=[]
     d=os.path.normpath(dir_local + dir_entry)
     try:
         os.makedirs(d)
@@ -89,10 +107,13 @@ def sf_get_modified(dir_entry='/'):
     with open(d + '/.hash','a+') as f:
         for row in f:
             hash = row.split(' ', 1 )
-            h_lcl[hash[0]]=hash[1]
+            name = hash[1].rstrip()
+            h_lcl[hash[0]]=name
             h1.append(hash[0])
+            fl.append(safe_unicode(name))
     f.close()
-    subdirs_local=[name for name in os.listdir(d) if os.path.isdir(os.path.join(d, name)) and not name.endswith('.sdr')]
+    subdirs_local = [safe_unicode(name) for name in os.listdir(d) if os.path.isdir(os.path.join(d, name)) and not name.endswith('.sdr')]
+    files_real    = [safe_unicode(name) for name in os.listdir(d) if os.path.isfile(os.path.join(d, name))]
 
     jl=sf_ls_lib(dir_entry)
     subdirs_srv=[]
@@ -103,7 +124,8 @@ def sf_get_modified(dir_entry='/'):
         elif i['type'] == 'dir':
             p=dir_entry+i['name']
             subdirs_srv.append(i['name'])
-            dr,rm,dl,up=sf_get_modified(p+'/')
+            dr,rm,dl,up,ul=sf_get_modified(p+'/')
+            sf_ul(p,ul)
             sf_dr(p,dr)
             sf_rm(p,rm)
             sf_dl(p,dl)
@@ -111,6 +133,8 @@ def sf_get_modified(dir_entry='/'):
 
     f_lcl = set(h1)
     f_srv = set(h2)
+    f_hash = set(fl)
+    f_real = set(files_real)
 
     d_lcl = set(subdirs_local)
     d_srv = set(subdirs_srv)
@@ -120,6 +144,9 @@ def sf_get_modified(dir_entry='/'):
 
     to_erase    = f_lcl - f_srv
     to_download = f_srv - f_lcl
+    to_upload   = f_real - f_hash
+    f_ul = list(to_upload)
+
     f_rm=[]
     f_dl=[]
     for i in list(to_erase):
@@ -129,7 +156,7 @@ def sf_get_modified(dir_entry='/'):
     for i in list(to_download):
         if i in h_srv.keys():
             f_dl.append(h_srv[i])
-    return d_rm, f_rm , f_dl, h_srv;
+    return d_rm, f_rm , f_dl, h_srv, f_ul;
 
 def sf_dl(dir_entry, dl_list):
     cclear(0,2,40)
@@ -189,7 +216,26 @@ def sf_up(dir_entry, up_list):
     cout(20,2,'OK')
     return;
 
+# Upload file
+def sf_ul(dir_entry, file):
+    cprint('[NOT implemented] Uploading: ',2)
+    hdr = { 'Authorization' : 'Token ' + token  , 'Accept' : 'application/json; indent=4'}
+    uurl = url + '/api2/repos/' + libid + '/upload-link/'
+    #print 'Upload URL: ',uurl
+    r = requests.get(uurl, headers=hdr, verify=ca_verify)
+    upload_link = r.json()
+    ## TODO: make a proper post request
+    ##print j
+    ##response = requests.post(
+    ##    upload_link, data={'filename': 'git.jpg', 'parent_dir': '/'},
+    ##    files={'file': open('/Users/xiez/Pictures/git.jpg', 'rb')},
+    ##    headers={'Authorization': 'Token {token}'. format(token=token)}
+    ##)
+    cout(20,2,'OK')
+    return;
+
 ### --- Main start
+requests.packages.urllib3.disable_warnings(SubjectAltNameWarning)
 config = ConfigParser.RawConfigParser()
 cfg_file = cfg_dir + '/seafile.cfg'
 config.read( cfg_file )
@@ -230,8 +276,9 @@ cprint ('Got ' + rc + ' from server', 1 )
 
 libid=sf_get_lib_id()
 
-dr,rm,dl,up = sf_get_modified()
+dr,rm,dl,up,ul = sf_get_modified()
 
+sf_ul('/',ul)
 sf_dr('/',dr)
 sf_rm('/',rm)
 sf_dl('/',dl)
